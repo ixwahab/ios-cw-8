68f4450d5efa4cbb134a675b1ee8542681d6ff61
Abdulwahab Alqallaf <nnoz0c@gmail.com>
2022-03-12 12:58:45 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,15 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
+var name = "abdulwahab"
+var birthdayyear = 2006
+var age = 2022 - 2006
+print ("Hello my name is \(name) and my age is \(age)")
+
+var textNumber = "8"
+var num2 = 4
+var num3 = 2.5
+var num1 = (Double(num2)) + num3
+
+num2 + (Int(textNumber))!
 
